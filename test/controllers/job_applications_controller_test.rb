b1b0620b68f262326c8157544c398b4d0e4db26a
lajora <lara.jr.keen@gmail.com>
2022-11-11 17:29:42 +0100

    application methods, 4th attempt solving testing errors, still need to change index to job and not user

--- test/controllers/job_applications_controller_test.rb
+++ test/controllers/job_applications_controller_test.rb
@@ -3,12 +3,12 @@ require "test_helper"
 class JobApplicationsControllerTest < ActionDispatch::IntegrationTest
 
   test "should get index" do
-    get job_applications_index_path
+    get job_job_applications_path(:job_id)
     assert_response :success
   end
 
   test "should get show" do
-    get job_applications_show_path # !!!!we need to change path name because too similar to index!!!!
+    get job_job_application_path(:job_id, :job_application_id) # !!!!we need to change path name because too similar to index!!!!
     assert_response :success
   end
 end
--- test/controllers/jobs_controller_test.rb
+++ test/controllers/jobs_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class JobsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get jobs_index_path
+    get jobs_path
     assert_response :success
   end
 end
