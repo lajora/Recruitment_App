63dfea9603ad504f915ef876b2873e4a83157391
lajora <lara.jr.keen@gmail.com>
2022-11-14 12:49:02 +0100

    added admin/job_applications admin/jobs controllers

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,14 @@
 class AdminController < ApplicationController
+
+before_filter :authorized?
+
+# methods
+
+    private
+    def authorized?
+      unless current_user.has_role? :admin
+        flash[:error] = "You are not authorized to view that page."
+        redirect_to root_path
+      end
+    end
 end
