4b099b6f5e965bb44e72f0cbd892ab38f4179896
VBenitoS <victorbenito8@gmail.com>
2022-11-14 18:03:22 +0100

    Finished seeding + added status and stage validations

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -2,4 +2,8 @@ class JobApplication < ApplicationRecord
     belongs_to :user
     belongs_to :job
     has_many :comments
+    validates :status, inclusion: { in: %w(pending accepted rejected), 
+            message: "%{value} is not a valid status" }
+    validates :stage, inclusion: { in: ['Application recieved', 'Screening call', 
+            "Technical test", 'Interview', 'Hired']}
 end
