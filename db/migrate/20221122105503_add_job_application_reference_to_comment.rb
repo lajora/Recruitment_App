8de0fdf4314c5825de88aa4ac27230dbb7d5cf3e
lajora <lara.jr.keen@gmail.com>
2022-11-22 11:58:03 +0100

    added foreignkey

--- db/migrate/20221122105503_add_job_application_reference_to_comment.rb
+++ db/migrate/20221122105503_add_job_application_reference_to_comment.rb
@@ -1,5 +1,5 @@
 class AddJobApplicationReferenceToComment < ActiveRecord::Migration[6.1]
   def change
-    add_reference :comments, :job_application, index: true
+    add_reference :comments, :job_application, index: true, foreign_key: true, null: false
   end
 end
